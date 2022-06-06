82b68462b31390200c48bce0ab332c72866ae626
KacperGaweda02 <105288861+KacperGaweda02@users.noreply.github.com>
2022-06-06 14:34:30 +0200

    zadanie 7

---
+++ Library/src/main/java/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    public String title;
+    public String author;
+    public String genre;
+    public String descrip;
+    public Book(String title, String author, String genre, String descrip) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.descrip = descrip;
+    }
+}
